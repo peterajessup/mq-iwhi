515806ae35f44d5e2a255698824bc088476834a1
peterajessup <peterajessup@users.noreply.github.com>
2022-10-21 10:10:44 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,17 +3,17 @@ oc project mq
 set +e
 # Remove the runnning queue manager instance (if any)
 
-oc delete QueueManager mq2
+oc delete QueueManager mq-ams
 
 # Delete the route object and secret for the QueueManager keystore (if any), and the mqsc configMap
-oc delete route mq2route
-oc delete secret mq2key
-oc delete configMap mq2-mqsc
+oc delete route mq-amsroute
+oc delete secret mqamskey
+oc delete configMap ams-mqsc
 oc delete persistentvolumeclaim data-mq2-ibm-mq-0 
 set -e
 # Create the route and the keystore secret and mqsc configMap
-oc apply -f mq2Route.yaml
-oc create secret tls mq2key --cert=./tls/tls.crt --key=./tls/tls.key
+oc apply -f mq-amsRoute.yaml
+oc create secret tls mqamskey --cert=./tls/tls.crt --key=./tls/tls.key
 oc create -f mqsc/mqsc.yaml
 
 set -e
