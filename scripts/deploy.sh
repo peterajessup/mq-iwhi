dd47905bc7d84074f29a916c7f672594a8182dfc
peterajessup <peterajessup@users.noreply.github.com>
2025-10-15 08:49:50 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,13 +6,13 @@ set +e
 # oc delete QueueManager iwhiqm
 
 # Delete the route object and secret for the QueueManager keystore (if any), and the mqsc configMap
-oc delete route mq-route
+oc delete route mq-iwhiroute
 oc delete secret mqkey
 oc delete configMap iwhi-mqsc
 
 set -e
 # Create the route and the keystore secret and mqsc configMap
-oc apply -f mq-amsRoute.yaml
+oc apply -f -mqRoute.yaml
 oc create secret tls mqkey --cert=./tls/tls.crt --key=./tls/tls.key
 
 oc create -f mqsc/mqsc.yaml
